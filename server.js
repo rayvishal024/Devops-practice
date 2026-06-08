c1c3d861c6be7b25a6856f1f1b722c2270cf62ca
rayvishal <rayvishal2005@gmail.com>
2026-06-08 17:09:15 +0530

    chore: Initilized project

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 7000;
+
+app.get("/", (req, res) => {
+     res.status(200).json({
+       message : "Hello from DevOps Bootcamp"
+  })
+});
+
+app.get('/health', (req, res) => {
+  res.status(200).json({
+    status: 'OK',
+    uptime: process.uptime(),
+       timestamp: new Date(),
+    message: 'Server is healthy'
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
